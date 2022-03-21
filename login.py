ff5bca923989a531e622150509effaf7ab2123ae
Samuello <milankovics.samuello@gmail.com>
2022-03-21 13:20:38 +0100

    icons

---
+++ login.py
@@ -0,0 +1,12 @@
+from tkinter import *
+ablak1 = Tk()
+gyoker = 'H:\\Python IKT\\'
+ablak1.geometry('600x600')
+ablak1.title('IKT Tkinter')
+icon = PhotoImage(file='D:\IKT python projects\IKT-python\tkinter_black.png')
+ablak1.iconphoto(True, icon)
+ablak1.config(background="black")
+elsokep = PhotoImage(file=  gyoker+'siraly.png')
+
+
+ablak1.mainloop()
